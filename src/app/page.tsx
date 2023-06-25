9227e22bd8f440de75b2fe47b5a1b5e7dfd9dc6f
elli-ma <elli9225@gmail.com>
2023-06-25 14:39:51 +0800

    end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,74 @@
 
 import { useEffect, useState } from 'react';
 import { AProduct, FetchData } from './ApiData.type';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { Product } from '@/components/Product';
+import { FixedSizeGrid } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
+import useSwrInfinite from 'swr/infinite'
+
+const fetcher = ( url: string) => fetch(url).then(res => res.json()).then((r: FetchData) => r.api_data.aProduct)
+function getKey(pageIndex: number, previousPageData: AProduct[]) {
+  if (previousPageData && !previousPageData.length) return null // reached the end
+  return `https://api.lichi.com/category/get_category_product_list?category=clothes&shop=1&lang=1&limit=12&page=${pageIndex+1}`
+}
 
 export default function Home() {
+  const {data, size, setSize} = useSwrInfinite<AProduct[]>(getKey, fetcher)
 
-  const [items, setItems] = useState<AProduct[]>([]);
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("https://api.lichi.com/category/get_category_product_list?category=clothes&shop=1&lang=1&limit=12&page=2")
-      const data: FetchData = await response.json();
-      setItems(data.api_data.aProduct);
-    }
-    fetchData();
+    setSize(1)
   }, []);
 
-  async function fetchData() {
-    const response = await fetch("https://api.lichi.com/category/get_category_product_list?category=clothes&shop=1&lang=1&limit=12&page=" + (items.length / 12) + 1)
-    const data: FetchData = await response.json()
-    setItems([...items, ...data.api_data.aProduct])
-  }
-
   return (
-    <main className="container mx-auto p-2 justify-center items-center" >
-      <h1 className='text-[30px] py-9 uppercase tracking-wide'>Lichi</h1>
-      <InfiniteScroll
-        dataLength={items.length}
-        next={fetchData}
-        hasMore={true}
-        loader={<h4>Loading...</h4>}
-        className='flex flex-wrap gap-2 justify-center w-full'>
-        {items.map((item) => <Product item={item} key={item.id} />)}
-      </InfiniteScroll>
-    </main>
+    <main className="flex h-screen flex-col" >
+      <h1 className='text-[30px] py-9 uppercase tracking-wide ml-2'>Lichi</h1>
+      <div className='flex h-full w-full overflow-x-hidden'>
+        <AutoSizer className='place-content-center'>
+          {({ height, width }) => {
+            if (!data) { return <div>Loading..</div>}
+            let maxColumns = 4;
+            let columnWidth = Math.max(width / maxColumns, 360);
+            let columns = Math.floor(width / columnWidth);
+            let rowCount = Math.floor(size * 12 /columns)
+            return <FixedSizeGrid
+              columnCount={columns}
+              columnWidth={columnWidth}
+              rowCount={rowCount}
+              rowHeight={630}
+              height={height}
+              width={width}
+              style={{ overflowX: "hidden" }} >
+              {({ columnIndex, rowIndex, style }: { columnIndex: number, rowIndex: number, style: any }) => {
+                const itemIndex = columnIndex + Math.floor(columns) * rowIndex
+                const [page, pageIndex] = [Math.floor(itemIndex/ 12), itemIndex % 12]
+                const item = data.at(page)?.at(pageIndex)
+                if (rowIndex == rowCount - 1 && columnIndex == columns -1 ) setSize(size + 1)
+                if (!item) {
+                  return <div>loading...</div>
+                }
+  
+                return (
+                  <li className='flex justify-between' style={style}>
+                    <div className='grow-0 flex flex-col p-2 hover:opacity-70 h-[630px]' key={item.id} >
+                      <img className='h-[512px] w-[384px]' src={item.photos[0].thumbs['384_512']} alt="" />
+                      <div className='flex flex-col text-start pb-4 pt-1'>
+                        <span className='text-[12px] tracking-wide uppercase h-[30px]'> {item.name}</span>
+                        <div className='flex justify-between items-center'>
+                          <span className='uppercase tracking-wide font-normal'>{item.price}{item.currency.postfix_symbol}</span>
+                          <button className='py-2 px-3 bg-[#c7b2a8] uppercase tracking-wide text-white text-[12px]'>Добавить</button>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+
+
+                );
+              }}
+            </FixedSizeGrid>
+          }}
+        </AutoSizer>
+      </div>
+
+
+    </main >
   );
 }
