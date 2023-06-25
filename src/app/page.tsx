bddab709c754b7805fb45a46124ff57d099cab1b
elli-ma <elli9225@gmail.com>
2023-06-25 14:58:29 +0800

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,14 @@ import { FixedSizeGrid } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
 import useSwrInfinite from 'swr/infinite'
 
-const fetcher = ( url: string) => fetch(url).then(res => res.json()).then((r: FetchData) => r.api_data.aProduct)
+const fetcher = (url: string) => fetch(url).then(res => res.json()).then((r: FetchData) => r.api_data.aProduct)
 function getKey(pageIndex: number, previousPageData: AProduct[]) {
   if (previousPageData && !previousPageData.length) return null // reached the end
-  return `https://api.lichi.com/category/get_category_product_list?category=clothes&shop=1&lang=1&limit=12&page=${pageIndex+1}`
+  return `https://api.lichi.com/category/get_category_product_list?category=clothes&shop=1&lang=1&limit=12&page=${pageIndex + 1}`
 }
 
 export default function Home() {
-  const {data, size, setSize} = useSwrInfinite<AProduct[]>(getKey, fetcher)
-
-  useEffect(() => {
-    setSize(1)
-  }, []);
+  const { data, size, setSize } = useSwrInfinite<AProduct[]>(getKey, fetcher, { initialSize: 4 })
 
   return (
     <main className="flex h-screen flex-col" >
@@ -25,11 +21,11 @@ export default function Home() {
       <div className='flex h-full w-full overflow-x-hidden'>
         <AutoSizer className='place-content-center'>
           {({ height, width }) => {
-            if (!data) { return <div>Loading..</div>}
+            if (!data) { return <div>Loading..</div> }
             let maxColumns = 4;
             let columnWidth = Math.max(width / maxColumns, 360);
             let columns = Math.floor(width / columnWidth);
-            let rowCount = Math.floor(size * 12 /columns)
+            let rowCount = Math.floor(size * 12 / columns)
             return <FixedSizeGrid
               columnCount={columns}
               columnWidth={columnWidth}
@@ -40,13 +36,15 @@ export default function Home() {
               style={{ overflowX: "hidden" }} >
               {({ columnIndex, rowIndex, style }: { columnIndex: number, rowIndex: number, style: any }) => {
                 const itemIndex = columnIndex + Math.floor(columns) * rowIndex
-                const [page, pageIndex] = [Math.floor(itemIndex/ 12), itemIndex % 12]
+                const [page, pageIndex] = [Math.floor(itemIndex / 12), itemIndex % 12]
                 const item = data.at(page)?.at(pageIndex)
-                if (rowIndex == rowCount - 1 && columnIndex == columns -1 ) setSize(size + 1)
+                if (rowIndex == rowCount - 1 && columnIndex == columns - 1 && page + 1 == size) {
+                  setSize(size + 6)
+                }
                 if (!item) {
                   return <div>loading...</div>
                 }
-  
+
                 return (
                   <li className='flex justify-between' style={style}>
                     <div className='grow-0 flex flex-col p-2 hover:opacity-70 h-[630px]' key={item.id} >
